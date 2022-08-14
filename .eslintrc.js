1d599f0fdf3264d57a57f2d40d5c9a3df0dbe605
SergBily <sergBily.dev@gmail.com>
2022-08-14 20:59:17 +0300

    init: start project rslang

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    },
+    project: './tsconfig.json'
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": 2
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  settings: {
+  }
+};
