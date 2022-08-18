f517f6d5b20ae35ecbf85496ca81510ab61b3c49
EugeneTsalko <tefworkmail@gmail.com>
2022-08-18 15:51:12 +0300

    feat: add new user creation

--- src/components/controller/authorization/authorization.ts
+++ src/components/controller/authorization/authorization.ts
@@ -1,8 +1,40 @@
 /* eslint-disable linebreak-style */
-// import Loader from '../load';
+import { MethodEnum, UrlFolderEnum } from '../../../types/loadServerData/enum';
+import Loader from '../load';
 
-// class Authorization extends Loader {
-//   test() {
-//     console.log('auth');
-//   }
-// }
+class Authorization extends Loader {
+  emailInput = document.getElementById('email') as HTMLInputElement;
+
+  nameInput = document.getElementById('name') as HTMLInputElement;
+
+  passwordInput = document.getElementById('password') as HTMLInputElement;
+
+  async createNewUser() {
+    console.log('create new user');
+
+    super.load(
+      {
+        method: MethodEnum.post,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: `${this.emailInput.value}`,
+          name: `${this.nameInput.value}`,
+          password: `${this.passwordInput.value}`,
+        }),
+      },
+      [UrlFolderEnum.users],
+    );
+  }
+
+  listen() {
+    const loginBtn = document.getElementById('login-btn');
+    loginBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      this.createNewUser();
+    });
+  }
+}
+
+export default Authorization;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { App } from './components/app/app';
+import Authorization from './components/controller/authorization/authorization';
 import AppView from './components/view/appView';
 
 const app = new App();
@@ -6,3 +7,6 @@ app.start();
 
 const appView = new AppView();
 appView.drawAuthForm();
+
+const authorization = new Authorization();
+authorization.listen();
