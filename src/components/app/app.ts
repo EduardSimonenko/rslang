e8efef5fe4a105143c2dc027c404f28c27c58dec
EugeneTsalko <tefworkmail@gmail.com>
2022-08-19 10:19:51 +0300

    refactor: change auth call place

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,5 +1,7 @@
 import { DataWords } from '../../types/loadServerData/interfaces';
+import Authorization from '../controller/authorization/authorization';
 import Controller from '../controller/controller';
+import AppView from '../view/appView';
 
 export class App {
   private controller: Controller;
@@ -11,6 +13,11 @@ export class App {
   public async start(): Promise<void> {
     const response = (await this.controller.testquery('5e9f5ee35eb9e72bc21af4dd')) as Response;
     (await response.json()) as DataWords;
+
+    const appView = new AppView();
+    appView.drawAuthForm();
+    const authorization = new Authorization();
+    authorization.listen();
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { App } from './components/app/app';
-import Authorization from './components/controller/authorization/authorization';
-import AppView from './components/view/appView';
 
 const app = new App();
 app.start();
-
-const appView = new AppView();
-appView.drawAuthForm();
-
-const authorization = new Authorization();
-authorization.listen();
