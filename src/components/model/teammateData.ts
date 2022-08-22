f99cc9291f05a36a23447c75f2099fa55c3ef26c
Eduard Simonenko <edik.simonenko2019@gmail.com>
2022-08-22 20:26:46 +0300

    feat: add teammates object and interface for it

---
+++ src/components/model/teammateData.ts
@@ -0,0 +1,24 @@
+import Iteammate from '../../types/pageViewTypes/Iteammates';
+
+const teammates: Iteammate[] = [
+  {
+    name: 'first teammate',
+    pathToFoto: './assets/images/teammate-foto.png',
+    linkToGithub: '#',
+    pathToGithubFoto: './assets/images/github.png',
+  },
+  {
+    name: 'second teammate',
+    pathToFoto: './assets/images/teammate-foto.png',
+    linkToGithub: '#',
+    pathToGithubFoto: './assets/images/github.png',
+  },
+  {
+    name: 'third teammate',
+    pathToFoto: './assets/images/teammate-foto.png',
+    linkToGithub: '#',
+    pathToGithubFoto: './assets/images/github.png',
+  },
+];
+
+export default teammates;
---
+++ src/types/pageViewTypes/Iteammates.ts
@@ -0,0 +1,6 @@
+export default interface Iteammate {
+  name: string;
+  pathToFoto: string;
+  linkToGithub: string;
+  pathToGithubFoto: string;
+}
