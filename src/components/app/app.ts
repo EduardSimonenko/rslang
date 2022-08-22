40629910978732a9d47f4a7573643543990f1da8
EugeneTsalko <tefworkmail@gmail.com>
2022-08-22 16:37:05 +0300

    feat: add close, registration/login form toggle; refactor

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,5 +1,6 @@
 import { DataWords } from '../../types/loadServerData/interfaces';
 import Controller from '../controller/controller';
+import AppView from '../view/appView';
 
 export class App {
   private controller: Controller;
@@ -11,6 +12,9 @@ export class App {
   public async start(): Promise<void> {
     const response = (await this.controller.testquery('5e9f5ee35eb9e72bc21af4dd')) as Response;
     (await response.json()) as DataWords;
+
+    const appView = new AppView();
+    appView.startAuth();
   }
 }
 
--- src/components/view/authorization/auth-render.ts
+++ src/components/view/authorization/auth-render.ts
@@ -11,28 +11,55 @@ class AuthForm {
   }
 
   render(): void {
-    const authWrapper = this.NewElement.createNewElement('div', ['auth-wrapper']);
-    const authHeader = this.NewElement.createNewElement('h2', ['auth-header'], 'Регистрация');
-
+    const loginWrapper = this.NewElement.createNewElement('div', ['login-wrapper']);
     const loginForm = this.NewElement.createNewElement('form', ['login-form']);
+    const closeBtn = this.NewElement.createNewElement('div', ['login-form__close-btn']);
+    const closeBtnImg = this.NewElement.createNewElement('img', ['close-btn-img']);
+    this.NewElement.setAttributes(closeBtnImg, { src: '../../../assets/svg/close.svg', alt: 'close' });
+    this.NewElement.insertChilds(closeBtn, [closeBtnImg]);
+    const loginTitle = this.NewElement.createNewElement('div', ['login-form-title'], '<h2>Изучать слова удобнее, если у вас есть профиль</h2>');
+    const loginSubtitle = this.NewElement.createNewElement('div', ['login-form-title'], 'Вы получите доступ к долгосрочному хранению статистики, а также сможете формировать собственный словарь.');
+    const loginFormFields = this.NewElement.createNewElement('div', ['login-form-fields']);
     const emailInput = this.NewElement.createNewElement('input', ['login-form__input']);
-    this.NewElement.setAttributes(emailInput, { id: 'email', type: 'email', placeholder: 'E-mail' });
-    const nameInput = this.NewElement.createNewElement('input', ['login-form__input']);
-    this.NewElement.setAttributes(nameInput, { id: 'name', type: 'text', placeholder: 'Name' });
+    this.NewElement.setAttributes(emailInput, {
+      id: 'email', type: 'email', placeholder: 'E-mail', required: '',
+    });
+    const nameInput = this.NewElement.createNewElement('input', ['login-form__input', 'hidden']);
+    this.NewElement.setAttributes(nameInput, {
+      id: 'name', type: 'text', placeholder: 'Имя',
+    });
     const passwordInput = this.NewElement.createNewElement('input', ['login-form__input']);
-    this.NewElement.setAttributes(passwordInput, { id: 'password', type: 'password', placeholder: 'Password' });
-    const registrationBtn = this.NewElement.createNewElement('button', ['btn', 'registration-btn'], 'Регистрация');
+    this.NewElement.setAttributes(passwordInput, {
+      id: 'password', type: 'password', placeholder: 'Пароль', required: '',
+    });
+    this.NewElement.insertChilds(loginFormFields, [emailInput, nameInput, passwordInput]);
+    const loginFormError = this.NewElement.createNewElement('div', ['login-form-error']);
+    const registrationBtn = this.NewElement.createNewElement('button', ['btn', 'registration-btn', 'hidden'], 'Регистрация');
     this.NewElement.setAttributes(registrationBtn, { id: 'registration-btn', type: 'submit' });
-    const loginBtn = this.NewElement.createNewElement('button', ['btn', 'login-btn'], 'Войти(мэйл и пасс)');
+    const loginBtn = this.NewElement.createNewElement('button', ['btn', 'login-btn'], 'Войти');
     this.NewElement.setAttributes(loginBtn, { id: 'login-btn', type: 'submit' });
     const cancelBtn = this.NewElement.createNewElement('button', ['btn', 'cancel-btn'], 'Отменить');
     this.NewElement.setAttributes(cancelBtn, { id: 'cancel-btn', type: 'button' });
-    const logoutBtn = this.NewElement.createNewElement('button', ['btn', 'logout-btn'], 'Выйти из аккаунта');
+    const logoutBtn = this.NewElement.createNewElement('button', ['btn', 'logout-btn', 'hidden'], 'Выйти из аккаунта');
     this.NewElement.setAttributes(logoutBtn, { id: 'logout-btn', type: 'button' });
-    this.NewElement.insertChilds(loginForm, [emailInput, nameInput, passwordInput, registrationBtn, loginBtn, cancelBtn, logoutBtn]);
-    this.NewElement.insertChilds(authWrapper, [authHeader, loginForm]);
+    const loginAuthChange = this.NewElement.createNewElement('div', ['login-form-registration'], 'Нет аккаунта? | Регистрация');
+    this.NewElement.insertChilds(loginForm, [closeBtn, loginTitle, loginSubtitle, loginFormFields, loginFormError, registrationBtn, loginBtn, cancelBtn, logoutBtn, loginAuthChange]);
+    loginWrapper.appendChild(loginForm);
+    document.body.appendChild(loginWrapper);
 
-    document.body.appendChild(authWrapper);
+    closeBtnImg.addEventListener('click', () => {
+      document.body.removeChild(loginWrapper);
+    });
+    loginAuthChange.addEventListener('click', () => {
+      nameInput.classList.toggle('hidden');
+      loginBtn.classList.toggle('hidden');
+      registrationBtn.classList.toggle('hidden');
+      if (loginAuthChange.innerHTML === 'Нет аккаунта? | Регистрация') {
+        loginAuthChange.innerHTML = 'Есть аккаунт? | Войти';
+      } else {
+        loginAuthChange.innerHTML = 'Нет аккаунта? | Регистрация';
+      }
+    });
   }
 }
 
