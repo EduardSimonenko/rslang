c9edd1f6dfcf5b1abb67616635ea0f901d81a9cb
EugeneTsalko <tefworkmail@gmail.com>
2022-08-30 15:51:36 +0300

    fix: delete from app

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,9 +1,7 @@
-import AppView from '../view/appView';
 import Page from '../view/pageView/mainPageView';
 
 export default class App {
   static async start(): Promise<void> {
     Page.renderMainPage();
-    AppView.startAudiocall();
   }
 }
